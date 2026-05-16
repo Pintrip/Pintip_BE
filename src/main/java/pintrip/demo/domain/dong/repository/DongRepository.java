e9fb16e2f8d98754cd27840cdb9d09a1416f7105
chhezue <chhezue@gmail.com>
2026-05-17 03:33:40 +0900

    Fix[#12] 폴더 구조 변경

--- src/main/java/pintrip/demo/domain/dong/repository/DongRepository.java
+++
@@ -1,9 +0,0 @@
-package pintrip.demo.domain.dong.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pintrip.demo.domain.dong.entity.Dong;
-import java.util.List;
-
-public interface DongRepository extends JpaRepository<Dong, Long> {
-    List<Dong> findAllByActiveTrue();
-}
