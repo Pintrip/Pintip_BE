107c777714383fef2ef4e10fe9447498114a7f7d
chhezue <chhezue@gmail.com>
2026-05-17 00:01:19 +0900

    Feat[#12] Beanstalkk & rds 배포

--- src/main/java/pintrip/demo/DemoApplication.java
+++ src/main/java/pintrip/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package pintrip.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "pintrip")
 public class DemoApplication {
 
 	public static void main(String[] args) {
